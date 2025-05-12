f6b35691eaeb78e7baf744312384ffc04feaa0ab
JB <ultsjbn@gmail.com>
2025-05-13 01:58:45 +0800

    testing commit

--- code/ReadFile.java
+++ code/ReadFile.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 // File Reader Source: https://www.w3schools.com/java/java_files_read.asp
 public class ReadFile {
     public static void main(String[] Args) {
+
+        System.out.println("-------------------------------");
+        System.out.println("-------------------------------");
+
         try {
             File Doc1 = new File("documents/test.txt");
             Scanner myReader = new Scanner(Doc1);
